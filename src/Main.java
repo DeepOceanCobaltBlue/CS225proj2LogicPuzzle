f697d055231b7e3d01240cfc09a21e91a5a5e8d8
CobaltyNSalty <96799127+CobaltyNSalty@users.noreply.github.com>
2023-02-04 16:48:09 -0500

    Initial

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+/* Work Log
+ * 2/4 [chris] - created class
+ *             - wrote class comment
+ *
+ */
+
+/** Main class will house the main method and launch a new instance of the logic game
+ *
+ */
+public class Main {
+    public static void main(String[] args) {
+        LogicGame game = new LogicGame();
+    }
+}
