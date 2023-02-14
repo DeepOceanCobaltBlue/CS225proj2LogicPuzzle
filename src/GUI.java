d7a204bbcf726bb74537553a93fdd8bc52da6ba1
CobaltyNSalty <96799127+CobaltyNSalty@users.noreply.github.com>
2023-02-14 11:42:46 -0500

    Reworked required classes

---
+++ src/GUI.java
@@ -0,0 +1,46 @@
+/*
+* Construct and return the GUI for
+* */
+
+import javax.swing.*;
+
+import static javafx.scene.input.KeyCode.J;
+
+public class GUI {
+    // FIELDS
+    /* Reference to finished graphical interface */
+    private JFrame rootFrame;
+    /* Reference game instance graphical requirements */
+    private GameBoard gameBoard;
+
+    // CONSTRUCTOR
+    public GUI() {
+        this.rootFrame = new JFrame(" Logic Puzzle Game ");
+    }
+
+    public GUI(GameBoard board) {
+        this();
+        this.gameBoard = board;
+        createGUI();
+    }
+
+    // METHODS
+    private void createGUI() {
+        /* Holds a row of blocks */
+        JPanel rowOneOfBlocks = new JPanel();
+        /*  */
+        JPanel rowOneFull = new JPanel();
+        /*  */
+        JPanel rowTwoOfBlocks = new JPanel();
+        /*  */
+        JPanel rowTwoFull = new JPanel();
+        /*  */
+        JPanel blocksPanel = new JPanel();
+
+    }
+
+    // ACCESSORS
+    public JFrame getDisplay() {
+        return rootFrame;
+    }
+}
