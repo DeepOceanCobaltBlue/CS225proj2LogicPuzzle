5bc4fa3baa64fe1de418da24ee02753ddd72d351
Andrew <spezzhh@gmail.com>
2023-02-16 12:05:10 -0500

    V1 of GameBoard.java, needs more editing (class comments, info comments) but barebones functions should call properly

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -1,2 +1,24 @@
 public class GameBoard {
+
+    //  16 sqaures in game
+    //  3 blocks
+    //  attributes
+    private String[] clues = new String[4];
+    private String[] answers = new String[3];
+    private String filePath = "";
+
+    //  methods
+    public String[] getClues(){
+        return this.clues;
+    }
+    public String[] getAnswers(){
+        return answers;
+    }
+    public String getFilePath(){
+        return filePath;
+    }
+    public void setFilePath(String desiredPath){
+        this.filePath = desiredPath;
+    }
+
 }
