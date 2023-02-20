df6d23294c3f0c53c809a5eeee8e64304f900788
Spezzhh <spezzhh@gmail.com>
2023-02-20 15:34:06 -0500

    V2 of GameBoard, furnished with methods and commments

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -1,95 +1,39 @@
 /* Work Log
- * 2/16 [Andrew]- created class
+ * 2/16 [Andrew]  - created class
  *              - wrote class comment
  *              - wrote version 1.0 of the class and added documentation
- * 2/18 [chris] - removed filepath from game board and added a parameterized constructor
- *              - updated class comment to reflect change, updated comments
- */
+ /
 
-import javax.swing.*;
-import java.util.ArrayList;
 
-/** Data structure for Logic Game assets
- * Contains all the textual information necessary to present to the player. Also
- * holds the Block objects that make up the game interface and the control
- * function buttons handled in the LogicGame class.
+/ Gameboard.java Comment
+This class will be used by the PuzzleGame in order to properly build our Puzzle for the
+user to play. This class contains arrays of clues and answers, as well as the filepath
+that will be read in by the application in order to obtain said clues & answers.
+This class will store all of this information so the PuzzleGame class may instantiate a new,
+fully furnished Gameboard for the user to use.
  */
 public class GameBoard {
-    // __ ATTRIBUTES __
-    /* Clues are the initial information given to the player */
-    private String[] clues;
-    /* The correct relationships between the 3 categories */
-    private String[] answers;
-    /* Initial prompt to the player which will give context to the puzzle */
-    private String story;
-    /* User saved notes used in game */
-    private String notes;
-    /* Interactive game assets */
-    private Block[][] blocks;
-    /* Reference used to pass control buttons to GUI in order to display.
-     * However, logic for controls are handled in the LogicGame class */
-    private JButton[] controls;
 
-    // __ CONSTRUCTORS __
-    public GameBoard() {
-        this.clues = null;
-        this.answers = null;
-        this.story = null;
-        this.blocks = null;
-        this.notes = null;
-    }
+    //  16 sqaures in game
+    //  3 blocks
 
-    public GameBoard(String[] clue, String[] answers, String story, Block[][] blocks) {
-        this.clues = clue;
-        this.answers = answers;
-        this.story = story;
-        this.blocks = blocks;
-        this.notes = null;
-    }
+    //  attributes
+    private String[] clues = new String[4]; //  Array to hold clues
+    private String[] answers = new String[3]; // Array to hold answers
+    private String filePath = ""; //    String to contain the filePath
 
-    private ArrayList<Block> findIncorrectBlocks(boolean includeEmpty) {
-        ArrayList<Block> incBlocks = new ArrayList<Block>();
-
-        for (Block[] blRow : blocks) {
-            for (Block block : blRow)
-                if (block != null) {
-                    if (block.anyErrors(includeEmpty)) {
-                        incBlocks.add(block);
-                    }
-                }
-            }
-
-        return incBlocks;
-    }
-
-    // GameBoard is our Block handler and should have to be used whenever LogicGame needs Block access.
-    // TODO: Allow GiveHint to detect if there are any incorrect/empty squares in blocks.
-    // TODO: Allow GiveHint to give the appropriate hint if squares are incorrect/empty.
-
-    // __ ACCESSORS __
-    public String[] getClues(){
+    //  methods
+    public String[] getClues(){ //  getter for the clues array
         return this.clues;
     }
-    public String[] getAnswers() {
-        return this.answers;
+    public String[] getAnswers(){ //    getter for the answers array
+        return answers;
     }
-    public String getStory() {
-        return this.story;
+    public String getFilePath(){ // getter for the filepath directory
+        return filePath;
     }
-    public Block[][] getBlocks() {
-        return blocks;
-    }
-    public void setControls(JButton[] controls) {
-        this.controls = controls;
-    }
-    public JButton[] getControls() {
-        return controls;
-    }
-    public String getNotes() {
-        return notes;
+    public void setFilePath(String desiredPath){ // setter for the filepath directory
+        this.filePath = desiredPath;
     }
 
-    public void setNotes(String notes) {
-        this.notes = notes;
-    }
 }
